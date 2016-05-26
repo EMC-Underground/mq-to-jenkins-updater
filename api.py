9075ac2d267e6e635f513a383493e84cfa9484d0
JP Quicksall <john.paul.quicksall@gmail.com>
2016-05-25 20:28:33 -0700

    fixed evaluation statement

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ port = int(os.getenv('VCAP_APP_PORT', 8080))
 def index_route():
   return 'Welcome to the jenkins updater api'
 
-if __name__ = '__main__':
+if __name__ == '__main__':
   scheduler.add_job(methods.get_message_from_SQS, 'interval', seconds = 10)
   scheduler.add_listener(methods.error_listener, events.EVENT_JOB_EXECUTED | events.EVENT_JOB_ERROR)
   scheduler.start()
